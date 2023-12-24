8df4b24238fd0b564823a75f3515f094a13511b5
Bram Schoenmakers <me@bramschoenmakers.nl>
2023-12-24 07:55:11 +0100

    Add group keyword to customizables

--- kagi.el
+++ kagi.el
@@ -61,18 +61,21 @@
 
 The token can be generated inside your account at
 https://kagi.com/settings?p=api"
-  :type '(choice string function))
+  :type '(choice string function)
+  :group 'kagi)
 
 (defcustom kagi-api-fastgpt-url "https://kagi.com/api/v0/fastgpt"
   "The Kagi FastGPT API entry point."
-  :type '(choice string function))
+  :type '(choice string function)
+  :group 'kagi)
 
 (defvar kagi--canned-response "{\"meta\":{\"id\":\"2c6ea22b-16ea-4529-87c0-612474125724\",\"node\":\"europe-west4\",\"ms\":3099,\"api_balance\":4.805},\"data\":{\"output\":\"Based on the context provided:\\n\\nKagi search is a premium search engine created by Kagi to be faster than other search engines like Google. Some context mentions using Kagi search instead of Google [1][2]. Emacs allows customization and extension through editing Lisp code. Kagi search could potentially be integrated into Emacs to provide search capabilities without leaving the editor, allowing users to seamlessly search from within Emacs. [3]\",\"tokens\":1036,\"references\":[{\"title\":\"View topic - can dnsmasq suppress AAAA/IPv6 queries? - DD-WRT\",\"snippet\":\"<b>Well</b>, that was <b>a</b> google nightmare! (Actually I use <b>kagi</b>.com now instead of google/startpage, fwiw.) I found nothing except the dnsmasq man ...\",\"url\":\"https://forum.dd-wrt.com/phpBB2/viewtopic.php?t=335561&sid=ca1d16c9ffe0db1a7ddd02ecc57f5a3e\"},{\"title\":\"View topic - [SOLVED: no] can dnsmasq suppress AAAA ... - DD-WRT\",\"snippet\":\"<b>Well</b>, that was <b>a</b> google nightmare! (Actually I use <b>kagi</b>.com now instead of google/startpage, fwiw.) I found nothing except the dnsmasq man ...\",\"url\":\"https://forum.dd-wrt.com/phpBB2/viewtopic.php?p=1294379&sid=cdc0b6cb7e86d7f3a66211f7f81d060f\"},{\"title\":\"Orion is a web browser based on Safari that supports both Chrome ...\",\"snippet\":\" It's from <b>Kagi</b> who are creating <b>a</b> \\\"Premium <b>search</b> engine\\\" currently ... Maybe I will <b>give</b> it <b>a go</b> in the future. Upvote 2. Downvote Reply reply\",\"url\":\"https://www.reddit.com/r/macapps/comments/sat03p/orion_is_a_web_browser_based_on_safari_that/\"}]}}"
   "A test response for debugging purposes.")
 
 (defcustom kagi-debug nil
   "Enable debugging statements."
-  :type 'boolean)
+  :type 'boolean
+  :group 'kagi)
 
 (defun kagi--build-curl-command (prompt)
   "Build a `curl' command to call the Kagi FastGPT API.
